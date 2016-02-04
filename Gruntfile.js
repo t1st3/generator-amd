10cf0f926b59e7a4a758853247d6dc0fdd678d7c
t1st3 <contact@tiste.org>
2016-02-04 22:23:52 +0100

    chore: replace T1st3 with t1st3

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,36 +2,36 @@
 
 module.exports = function (grunt) {
 
-	require('time-grunt')(grunt);
+  require('time-grunt')(grunt);
 
-	require('load-grunt-tasks')(grunt, {
-		scope: 'devDependencies',
-		config: 'package.json',
-		pattern: ['grunt-*']
-	});
+  require('load-grunt-tasks')(grunt, {
+    scope: 'devDependencies',
+    config: 'package.json',
+    pattern: ['grunt-*']
+  });
 
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		jshint: {
-			options: {
-				jshintrc: '.jshintrc',
-				reporter: require('jshint-stylish')
-			},
-			all: [
-				'app/index.js',
-				'Gruntfile.js'
-			]
-		},
-		jscs: {
-			src: ['app/index.js', 'Gruntfile.js'],
-			options: {
-				config: '.jscs.json'
-			}
-		}
-	});
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc',
+        reporter: require('jshint-stylish')
+      },
+      all: [
+        'app/index.js',
+        'Gruntfile.js'
+      ]
+    },
+    jscs: {
+      src: ['app/index.js', 'Gruntfile.js'],
+      options: {
+        config: '.jscs.json'
+      }
+    }
+  });
 
-	grunt.registerTask('default', [
-		'jshint',
-		'jscs'
-	]);
+  grunt.registerTask('default', [
+    'jshint',
+    'jscs'
+  ]);
 };
