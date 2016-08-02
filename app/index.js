accb590b29bfa922f3c358d787d4ba7777a831ea
t1st3 <contact@tiste.org>
2016-08-02 19:45:52 +0200

    refactor: for yeoman running context

--- app/index.js
+++ app/index.js
@@ -1,113 +1,88 @@
 'use strict';
 
 var util = require('util'),
-    base = require('yeoman-generator').Base,
-    figlet = require('figlet'),
-
+generators = require('yeoman-generator'),
+figlet = require('figlet'),
+updateNotifier = require('update-notifier'),
+pkg = require('../package.json'),
 AmdGenerator;
 
-AmdGenerator = module.exports = function AmdGenerator(args, options) {
-	base.apply(this, arguments);
-
-	this.on('end', function () {
-		this.installDependencies({ skipInstall: options['skip-install'] });
-	});
-
-	this.pkg = require('../package.json');
-};
-
-util.inherits(AmdGenerator, base);
-
-AmdGenerator.prototype.askFor = function askFor() {
-	var cb, t;
-
-	cb = this.async();
-
-	t = this;
-	figlet('yo amd', function (err, data) {
-		if (err) {
-			console.log('Something went wrong with figlet');
-			console.dir(err);
-			return;
-		} else {
-			console.log(data);
-			var updateNotifier, notifier, prompts;
-
-			updateNotifier = require('update-notifier');
-			notifier = updateNotifier(
-				{
-					pkg: t.pkg
-				}
-			);
-
-			if (notifier.update) {
-				notifier.notify();
-			}
-
-			if (t.yeoman) {
-				console.log(t.yeoman);
-			}
-
-			prompts = [
-				{
-					name: 'githubAccount',
-					message: 'What is your github account?'
-				},
-				{
-					name: 'moduleName',
-					message: 'What is the name of your AMD module (the slug-name of the Github repository)?'
-				},
-				{
-					name: 'objectName',
-					message: 'What is the name of the object?'
-				}
-			];
-
-			t.prompt(prompts, function (props) {
-				var slugify = function (text) {
-					return text.toString().toLowerCase()
-					.replace(/\s+/g, '-') // Replace spaces with -
-					.replace(/[^\w\-]+/g, '') // Remove all non-word chars
-					.replace(/\-\-+/g, '-') // Replace multiple - with single -
-					.replace(/^-+/, '') // Trim - from start of text
-					.replace(/-+$/, ''); // Trim - from end of text
-				};
-				t.githubAccount = props.githubAccount;
-				t.moduleName = props.moduleName;
-				t.moduleSlug = slugify(props.moduleName);
-				t.objectName = props.objectName;
-				t.objectSlug = slugify(props.objectName);
-				cb();
-			}.bind(t));
+AmdGenerator = module.exports = generators.Base.extend({
+	constructor: function () {
+		generators.Base.apply(this, arguments);
+		var done = this.async();
+		var notifier = updateNotifier({
+			pkg: pkg
+		});
+		if (notifier.update) {
+			notifier.notify();
 		}
-	});
-};
 
-AmdGenerator.prototype.app = function app() {
+		figlet('yo amd', function (err, data) {
+			if (err) {
+				done(err);
+			} else {
+				done();
+			}
+		});
+	},
+	prompting: function () {
+		return this.prompt([{
+			type: 'input',
+			name: 'githubAccount',
+			message: 'What is your github account?'
+		}, {
+			type: 'input',
+			name: 'moduleName',
+			message: 'What is the name of your AMD module (the slug-name of the Github repository)?'
+		}, {
+			type: 'input',
+			name: 'objectName',
+			message: 'What is the name of the object?'
+		}]).then(function (answers) {
+			this.log('github account', answers.githubAccount);
+			this.log('module name', answers.moduleName);
+			this.log('object name', answers.objectName);
+			var slugify = function (text) {
+				return text.toString().toLowerCase()
+				.replace(/\s+/g, '-') // Replace spaces with -
+				.replace(/[^\w\-]+/g, '') // Remove all non-word chars
+				.replace(/\-\-+/g, '-') // Replace multiple - with single -
+				.replace(/^-+/, '') // Trim - from start of text
+				.replace(/-+$/, ''); // Trim - from end of text
+			};
+			this.githubAccount = answers.githubAccount;
+			this.moduleName = answers.moduleName;
+			this.moduleSlug = slugify(answers.moduleName);
+			this.objectName = answers.objectName;
+			this.objectSlug = slugify(answers.objectName);
+		}.bind(this));
+	},
+	writing: function () {
+		this.mkdir('src');
+		this.template('src/_amd-module.js', 'src/' + this.moduleName + '.js');
 
-	this.mkdir('src');
-	this.template('src/_amd-module.js', 'src/' + this.moduleName + '.js');
+		this.mkdir('example');
+		this.template('example/_app.js', 'example/app.js');
+		this.template('example/_index.html', 'example/index.html');
+		this.copy('example/main.css', 'example/main.css');
+		this.copy('example/require-2.1.10.min.js', 'example/require-2.1.10.min.js');
 
-	this.mkdir('example');
-	this.template('example/_app.js', 'example/app.js');
-	this.template('example/_index.html', 'example/index.html');
-	this.copy('example/main.css', 'example/main.css');
-	this.copy('example/require-2.1.10.min.js', 'example/require-2.1.10.min.js');
+		this.mkdir('dist');
+		this.mkdir('doc');
 
-	this.mkdir('dist');
-	this.mkdir('doc');
-
-	this.template('_gruntfile.js', 'Gruntfile.js');
-	this.copy('bowerrc', '.bowerrc');
-	this.template('_package.json', 'package.json');
-	this.template('_bower.json', 'bower.json');
-	this.template('_README.md', 'README.md');
-	this.copy('gitignore', '.gitignore');
-	this.copy('gitattributes', '.gitattributes');
-	this.copy('jscs.json', '.jscs.json');
-};
-
-AmdGenerator.prototype.projectfiles = function projectfiles() {
-	this.copy('editorconfig', '.editorconfig');
-	this.copy('jshintrc', '.jshintrc');
-};
+		this.template('_gruntfile.js', 'Gruntfile.js');
+		this.copy('bowerrc', '.bowerrc');
+		this.template('_package.json', 'package.json');
+		this.template('_bower.json', 'bower.json');
+		this.template('_README.md', 'README.md');
+		this.copy('gitignore', '.gitignore');
+		this.copy('gitattributes', '.gitattributes');
+		this.copy('jscs.json', '.jscs.json');
+		this.copy('editorconfig', '.editorconfig');
+		this.copy('jshintrc', '.jshintrc');
+	},
+	install: function () {
+		this.installDependencies({ skipInstall: this.option('skip-install') });
+	}
+});
